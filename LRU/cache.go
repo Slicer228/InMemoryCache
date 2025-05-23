b5a818e72fe05e30bb25abfc6913ff167c5900cb
Slicer228 <timofeyg05@mail.ru>
2025-05-23 12:04:23 +0300

    Initial

---
+++ LRU/cache.go
@@ -0,0 +1,62 @@
+package LRU
+
+import (
+	"container/list"
+)
+
+type CachedFunc[K comparable, V any] func(K) (V, error)
+
+type LRUCache struct {
+	capacity uint32
+	data     map[interface{}]interface{}
+	elements *list.List
+}
+
+func (lru *LRUCache) Put(key interface{}, value interface{}) {
+	if _, exists := lru.data[key]; exists || lru.elements.Len() == int(lru.capacity) {
+		delete(lru.data, lru.elements.Back().Value.(interface{}))
+		lru.elements.Remove(lru.elements.Back())
+	}
+
+	lru.elements.PushFront(key)
+	lru.data[key] = value
+}
+
+func (lru *LRUCache) Get(key interface{}) interface{} {
+	if value, exists := lru.data[key]; exists {
+		lru.elements.MoveToFront(&list.Element{Value: value})
+		return value
+	} else {
+		return nil
+	}
+}
+
+func NewLRUDecorator[K comparable, V any](capacity uint32) func(CachedFunc[K, V]) CachedFunc[K, V] {
+	cache := &LRUCache{
+		capacity: capacity,
+		data:     make(map[interface{}]interface{}),
+		elements: list.New(),
+	}
+	return func(f CachedFunc[K, V]) CachedFunc[K, V] {
+		return func(parameter K) (V, error) {
+			if val := cache.Get(parameter); val != nil {
+				return val.(V), nil
+			} else {
+				result, err := f(parameter)
+				if err != nil {
+					return result, err
+				}
+				cache.Put(parameter, result)
+				return result, err
+			}
+		}
+	}
+}
+
+func New(capacity uint32) *LRUCache {
+	return &LRUCache{
+		capacity: capacity,
+		data:     make(map[interface{}]interface{}),
+		elements: list.New(),
+	}
+}
