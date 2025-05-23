0a6ac6ccf379666e9935ed7eebdfd37dde088101
Slicer228 <timofeyg05@mail.ru>
2025-05-23 12:43:45 +0300

    Initial

--- LRU/cache.go
+++ LRU/cache.go
@@ -2,6 +2,7 @@ package LRU
 
 import (
 	"container/list"
+	"sync"
 )
 
 type CachedFunc[K comparable, V any] func(K) (V, error)
@@ -10,9 +11,12 @@ type LRUCache struct {
 	capacity uint32
 	data     map[interface{}]interface{}
 	elements *list.List
+	mutex    sync.RWMutex
 }
 
 func (lru *LRUCache) Put(key interface{}, value interface{}) {
+	lru.mutex.Lock()
+	defer lru.mutex.Unlock()
 	if _, exists := lru.data[key]; exists || lru.elements.Len() == int(lru.capacity) {
 		delete(lru.data, lru.elements.Back().Value.(interface{}))
 		lru.elements.Remove(lru.elements.Back())
@@ -23,6 +27,8 @@ func (lru *LRUCache) Put(key interface{}, value interface{}) {
 }
 
 func (lru *LRUCache) Get(key interface{}) interface{} {
+	lru.mutex.RLock()
+	defer lru.mutex.RUnlock()
 	if value, exists := lru.data[key]; exists {
 		lru.elements.MoveToFront(&list.Element{Value: value})
 		return value
@@ -36,6 +42,7 @@ func NewLRUDecorator[K comparable, V any](capacity uint32) func(CachedFunc[K, V]
 		capacity: capacity,
 		data:     make(map[interface{}]interface{}),
 		elements: list.New(),
+		mutex:    sync.RWMutex{},
 	}
 	return func(f CachedFunc[K, V]) CachedFunc[K, V] {
 		return func(parameter K) (V, error) {
@@ -58,5 +65,6 @@ func New(capacity uint32) *LRUCache {
 		capacity: capacity,
 		data:     make(map[interface{}]interface{}),
 		elements: list.New(),
+		mutex:    sync.RWMutex{},
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import . "fmt"
+import (
+	"InMemoryCache/LRU"
+	. "fmt"
+)
+
+func sum(a int) (int, error) {
+	return a + a, nil
+}
 
 func main() {
 	Println("Hello from cache!")
+	decorator := LRU.NewLRUDecorator[int, int](5)
+	sumCached := decorator(sum)
+	Println(sumCached(1))
+	Println(sumCached(2))
+	Println(sumCached(3))
+
+	return
 }
