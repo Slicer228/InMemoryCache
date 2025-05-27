c6a59d2c8443ab9bca9341fdfe505b0a19bf31e9
Slicer228 <timofeyg05@mail.ru>
2025-05-27 14:24:56 +0300

    refactored LFU, mae o(1) time

--- LFU/cache.go
+++ LFU/cache.go
@@ -2,7 +2,6 @@ package LFU
 
 import (
 	"container/list"
-	"fmt"
 	"sync"
 )
 
@@ -13,9 +12,10 @@ type CachedFunc[K comparable, V any] func(K) (V, error)
 type LFUCache struct {
 	capacity               uint64                // max value of stored keys in cache
 	data                   map[any]*list.Element // map for O(1) access time to cached value
-	elements               *list.List            // Linked list for managing cached data
+	elements               map[uint64]*list.List // Map of linked list for managing cached data key is count of usages, value is list of element structs
 	mutex                  sync.RWMutex          // for synchronization
 	maxAccessCountToUpdate uint64                // the maximum number of cached values received in a row before updating
+	minKeyUsages           uint64                // minimum key of usage bucket
 }
 
 type element struct {
@@ -24,71 +24,73 @@ type element struct {
 	useCount uint64
 }
 
-func pushElemnetUp(elements *list.List, el *list.Element) {
-	// service function to move up element in list, based of quantity of calls
+func (lfu *LFUCache) migrateElementToNewBucket(el *list.Element) {
+	internalEl := el.Value.(*element)
 
-	for prev := el.Prev(); ; prev = prev.Prev() {
+	lfu.elements[internalEl.useCount].Remove(el)
+	delete(lfu.data, internalEl.key)
+	internalEl.useCount++
 
-		//check if cycle moved through all list
-		if prev == nil {
-			elements.MoveBefore(el, elements.Front())
-			break
-		}
+	// Create a new list if key doesn't exist
+	if lfu.elements[internalEl.useCount] == nil {
+		lfu.elements[internalEl.useCount] = list.New()
+	}
 
-		if el.Value.(*element).useCount > prev.Value.(*element).useCount {
-			continue
-		} else {
-			elements.MoveBefore(el, prev.Next())
-			break
-		}
+	lfu.data[internalEl.key] = lfu.elements[internalEl.useCount].PushFront(internalEl)
+
+	// Change minimal key of usages if last is empty now
+	if lfu.elements[internalEl.useCount-1].Len() == 0 {
+		delete(lfu.elements, internalEl.useCount-1)
+		lfu.minKeyUsages = internalEl.useCount
 	}
 }
 
-func (lru *LFUCache) Put(key any, value any) {
+func (lfu *LFUCache) deleteLFUElement() {
+	toDel := lfu.elements[lfu.minKeyUsages].Front()
+	delete(lfu.data, toDel.Value.(*element).key)
+	lfu.elements[lfu.minKeyUsages].Remove(toDel)
+}
 
-	lru.mutex.Lock()
-	defer lru.mutex.Unlock()
+func (lfu *LFUCache) Put(key any, value any) {
+
+	lfu.mutex.Lock()
+	defer lfu.mutex.Unlock()
 
 	//check if key already exists to update value
-	if el, exists := lru.data[key]; exists {
-		el.Value.(*element).useCount++
-		pushElemnetUp(lru.elements, el)
+	if el, exists := lfu.data[key]; exists {
 		el.Value.(*element).value = value
-		fmt.Println("After PUT")
-		for e := lru.elements.Front(); e != nil; e = e.Next() {
-			fmt.Print(e.Value, " ")
-		}
-		fmt.Print("\n")
+		lfu.migrateElementToNewBucket(el)
 		return
 	}
 
 	//check if cache need to delete less used cache to set new
-	if lru.elements.Len() == int(lru.capacity) {
-		delete(lru.data, lru.elements.Back().Value.(*element).key)
-		lru.elements.Remove(lru.elements.Back())
+	if len(lfu.data) == int(lfu.capacity) {
+		lfu.deleteLFUElement()
 	}
 
-	lru.elements.PushBack(&element{key: key, value: value, useCount: 1})
-	lru.data[key] = lru.elements.Back()
+	//check if bucket 1 exists else create
+	if lfu.elements[1] == nil {
+		lfu.elements[1] = list.New()
+	}
 
+	lfu.data[key] = lfu.elements[1].PushFront(&element{key: key, value: value, useCount: 1})
 }
 
-func (lru *LFUCache) Get(key any) any {
+func (lfu *LFUCache) Get(key any) any {
+	//return nil in case key not founded or value require update else return founded value
 
-	lru.mutex.RLock()
-	defer lru.mutex.RUnlock()
+	lfu.mutex.RLock()
+	defer lfu.mutex.RUnlock()
 
 	//check if key in cache exists to process receiving
-	if el, exists := lru.data[key]; exists {
+	if el, exists := lfu.data[key]; exists {
+
+		lfu.migrateElementToNewBucket(el)
 
 		//handling update of elem value
-		if el.Value.(*element).useCount%lru.maxAccessCountToUpdate == 0 {
+		if el.Value.(*element).useCount%lfu.maxAccessCountToUpdate == 0 {
 			return nil
 		}
-
-		el.Value.(*element).useCount++
-
-		pushElemnetUp(lru.elements, el)
 
 		return el.Value.(*element).value
 	} else {
@@ -100,9 +102,10 @@ func NewLFUDecorator[K comparable, V any](capacity, maxAccessCountToUpdate uint6
 	cache := &LFUCache{
 		capacity:               capacity,
 		data:                   make(map[any]*list.Element),
-		elements:               list.New(),
+		elements:               make(map[uint64]*list.List),
 		mutex:                  sync.RWMutex{},
 		maxAccessCountToUpdate: maxAccessCountToUpdate,
+		minKeyUsages:           1,
 	}
 	return func(f CachedFunc[K, V]) CachedFunc[K, V] {
 		return func(parameter K) (V, error) {
@@ -129,8 +132,9 @@ func New(capacity, maxAccessCountToUpdate uint64) *LFUCache {
 	return &LFUCache{
 		capacity:               capacity,
 		data:                   make(map[any]*list.Element),
-		elements:               list.New(),
+		elements:               make(map[uint64]*list.List),
 		mutex:                  sync.RWMutex{},
 		maxAccessCountToUpdate: maxAccessCountToUpdate,
+		minKeyUsages:           1,
 	}
 }
